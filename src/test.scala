946a5761d0ce27801480b6c6a550fd2b08e676c0
andrilareida <andrilareida@gmail.com>
2016-12-23 09:07:10 +0100

    Initial commit

---
+++ src/test.scala
@@ -0,0 +1,54 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by Andri on 21.12.2016.
+  */
+object test {
+  def main(args: Array[String]) {
+    // create Spark context with Spark configuration
+    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
+
+    val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
+
+    val month = 5
+    val day = 15
+    val maxTorrents = 50;
+    val peertorrents = sqlContext.sql("FROM torrentsperip SELECT infohash, peeruid " +
+      "WHERE year = 2016" +
+      "month = " + month +
+      " AND day = " + day +
+      " AND ip NOT EXISTS IN " +
+      "(select ip FROM dailysharedtorrents " +
+      " WHERE year = 2016" +
+      " AND month = " + month +
+      " AND day = " + day +
+      " AND shared <= " + maxTorrents + ")")
+
+    peertorrents.map(pt => (pt(1), pt(0))).groupByKey()
+      .flatMap{case (peer: String, hashes: Iterable[String]) =>
+        permutation(hashes).map(edge => (edge, 1))}.reduceByKey(_ + _)
+      .saveAsTextFile("/user/viola/torrentnet/"+month+"/"+day+"/")
+
+  }
+
+  def permutation( iter:Iterable[String] ) : Array[String] ={
+    val list = iter.toArray[String]
+    var s = ArrayBuffer.empty[String]
+    for (x <- 0 to (list.length - 2)) {
+      val first = list(x)
+      list.drop(x + 1).foreach(blah => {
+        if (first.compareTo(blah) < 0) {
+          //println(first + "\t" + blah)
+          s+=(first + "\t" + blah)
+        } else if (first.compareTo(blah) > 0) {
+          // println(blah + "\t" + first)
+          s+= (blah + "\t" + first)
+        }
+      })
+    }
+    return s.toArray
+  }
+
+}
