0f1d45156848288af0b82e758ca735f1f822e770
andrilareida <andrilareida@gmail.com>
2016-12-23 13:07:12 +0100

    sql edit

--- src/test.scala
+++ src/test.scala
@@ -1,3 +1,5 @@
+import java.util.{Calendar, Date, GregorianCalendar}
+
 import org.apache.log4j.LogManager
 import org.apache.log4j.nt.NTEventLogAppender
 import org.apache.spark.sql.catalyst.expressions.Log
@@ -13,27 +15,37 @@ object test {
     // create Spark context with Spark configuration
     val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
-   // val log = LogManager.getRootLogger
-    val month = 5
-    val day = 15
-    val year = 2016
+    val log = LogManager.getRootLogger
+
     val maxTorrents = 50
-    val query = "FROM torrentsperip A, dailysharedtorrents B" +
-      " SELECT A.infohash, A.peeruid " +
-      " WHERE A.peeruid = B.peeruid" +
-      " AND B.year = A.year = " + year +
-      " AND B.month = A.month = " + month +
-      " AND B.day = B.day = " + day +
-      " AND B.shared <= " + maxTorrents +
-      " GROUP BY A.infohash, A.peeruid"
-   // log.info(query)
-    val peertorrents = sqlContext.sql(query)
+    val year = 2016
+    val month = 5 to 7
+    month.foreach(m => {
+      val cal = new GregorianCalendar()
+      cal.set(year, m-1, 1)
+      val days = 1 to cal.getActualMaximum(Calendar.DAY_OF_MONTH)
+      days.foreach(day => {
+        val query = "FROM torrentsperip A, dailysharedtorrents B" +
+          " SELECT A.infohash, A.peeruid " +
+          " WHERE A.peeruid = B.peeruid" +
+          " AND B.year = A.year = " + year +
+          " AND B.month = A.month = " + m +
+          " AND B.day = B.day = " + day +
+          " AND B.shared BETWEEN 1 AND " + maxTorrents +
+          " GROUP BY A.infohash, A.peeruid"
+        log.info(query)
+        val peertorrents = sqlContext.sql(query)
+
+        peertorrents.map(pt => (pt(1), pt(0))).groupByKey()
+          .flatMap{case (peer: String, hashes: Iterable[String]) =>
+            permutation(hashes).map(edge => (edge, 1))}.reduceByKey(_ + _)
+          .saveAsTextFile("/user/viola/torrentnet/"+m+"/"+day+"/")
+      })
+    })
+    val day = 15
 
 
-    peertorrents.map(pt => (pt(1), pt(0))).groupByKey()
-      .flatMap{case (peer: String, hashes: Iterable[String]) =>
-        permutation(hashes).map(edge => (edge, 1))}.reduceByKey(_ + _)
-      .saveAsTextFile("/user/viola/torrentnet/"+month+"/"+day+"/")
+
 
   }
 
