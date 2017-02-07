d81d85351fbe60f75fe8ca3363b7572227552694
andrilareida <andrilareida@gmail.com>
2017-02-07 12:58:15 +0100

    adapted test

--- test/ASNetWeightedTest.scala
+++ test/ASNetWeightedTest.scala
@@ -8,18 +8,23 @@ class ASNetWeightedTest extends FunSuite{
 
   test("An AS permutation should produce all edges and"){
 
-    val size = 1 * 1024 * 1024
+    val size = 1 * 1024 * 1024 * 1024
 
     val iter = Iterable(ASrecord(1,20,size),
-      ASrecord(2,10,size),
-      ASrecord(3,5,size))
+      ASrecord(2,5,size),
+      ASrecord(3,5,size),
+      ASrecord(4,10,size),
+      ASrecord(5,1,size))
     val edges = ASNetWeighted.permutation(iter)
 
-    assert(edges.length == math.pow(iter.size,2))
+    assert(edges.length == iter.size*(iter.size-1)/2)
+    val totalPeers = iter.map(_.peers).sum
+    //Calculate the total downloads per as and subtract the part that is served internally. Divide by two
+    val totalWeight=iter.map(x => x.size * x.peers - x.size * x.peers * x.peers/totalPeers).sum/(2 * math.pow(1024,3))
 
-    val totalWeight=iter.map(x => x.size *x.peers).sum/math.pow(1024,3)
-    assert(edges.map(_.weight).sum == totalWeight)
     edges.foreach(println(_))
+    assert(edges.map(_.weight).sum.round == totalWeight.round)
+
   }
 
 
